3ac7da0f34c6cf870dc62fa46e66c8c0f64d7901
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-26 12:36:20 +0200

    add dependencies

---
+++ src/test/java/luca/sberna/capstone/empire/of/gamers/ApplicationTests.java
@@ -0,0 +1,13 @@
+package luca.sberna.capstone.empire.of.gamers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
