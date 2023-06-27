4815942663830ccd6c89c290acf45c5a99b7224f
Luca-Sberna <sberna.luca.ibiza@gmail.com>
2023-06-27 17:20:13 +0200

    added pagination and test for pagination

--- src/main/java/luca/sberna/capstone/empire/of/gamers/services/CreditCardService.java
+++ src/main/java/luca/sberna/capstone/empire/of/gamers/services/CreditCardService.java
@@ -1,11 +1,14 @@
 package luca.sberna.capstone.empire.of.gamers.services;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import luca.sberna.capstone.empire.of.gamers.entities.CreditCard;
@@ -51,8 +54,14 @@ public class CreditCardService {
 		return optionalCreditCard.orElse(null);
 	}
 
-	public List<CreditCard> getAllCreditCards() {
-		return creditCardRepository.findAll();
+	public Page<CreditCard> getAllCreditCards(int page, int size, String sortBy) {
+		if (size < 0)
+			size = 10;
+		if (size > 100)
+			size = 20;
+		Pageable pageable = PageRequest.of(page, size, Sort.by(sortBy));
+
+		return creditCardRepository.findAll(pageable);
 	}
 
 	public CreditCard updateCreditCard(UUID id, CreditCardRegistrationPayload payload) {
